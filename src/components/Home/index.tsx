891198c1285f6607e662746e84349956a516dd57
Henry Chen <henryq.chen101@gmail.com>
2024-06-20 20:06:45 -0400

    fixed blurb and animation

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import Icons from "@/components/Icons";
 import Photo from "@/components/Photo";
+
+import { motion } from "framer-motion";
+import { fadein } from "@/utils/fadein";
 
 export default function Home() {
   return (
@@ -8,22 +13,53 @@ export default function Home() {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           <div className="flex flex-col gap-3 text-center xl:text-left font-robotoSlab">
-            <span className="text-xl">Software Developer</span>
-            <h1 className="h2 font-gambarino">
+            <motion.span
+              variants={fadein({ direction: "up", delay: 0.3 })}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-xl"
+            >
+              Software Developer
+            </motion.span>
+            <motion.h1
+              variants={fadein({ direction: "up", delay: 0.4 })}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="h2 font-gambarino"
+            >
               Hello I&apos;m <br />{" "}
-              <span className="h1 gradient-text text-transparent animate-gradient">
+              <motion.span
+                variants={fadein({ direction: "up", delay: 0.5 })}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+                className="h1 gradient-text text-transparent animate-gradient"
+              >
                 Henry Chen
-              </span>
-            </h1>
-            <p className="max-w-[500px] mb-9 text-white">
-              A freelance front-end developer and web designer helping startups
-              all around the world gain their unfair advantage. world gain their
-              unfair advantage.
-            </p>
+              </motion.span>
+            </motion.h1>
+            <motion.p
+              variants={fadein({ direction: "up", delay: 0.7 })}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="max-w-[500px] mb-9 text-white"
+            >
+              A passionate computer science student with an interest in
+              full-stack development and artificial intelligence. Constantly
+              learning, building, and pushing boundries.
+            </motion.p>
           </div>
-          <div>
+          <motion.div
+            variants={fadein({ direction: "up", delay: 0.5 })}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+          >
             <Photo />
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
